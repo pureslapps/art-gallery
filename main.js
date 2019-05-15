b37e761be61a65100abb6348ab7c4ec5fa59ea04
Jorge Bhagwandeen <jorgebhagwandeen@yahoo.com>
2019-05-15 16:53:05 -0400

    added basic outline

---
+++ main.js
@@ -0,0 +1,20 @@
+class User {
+  constructor(email, pasword) {
+  this.email = email
+  this.password = password
+  }
+}
+
+
+class Classic {
+  constructor(frame, art) {
+  this.frame = "black-border"
+  this.art = "art-url"
+
+  render(user) {
+
+    }
+  }
+}
+
+user1 = new User ("dave@show.com", "comedy")
